2f21a6a0797ca80dae56ba1428a3dd73d80fbb7e
Kirstin Horvat <horvatk@shaw.ca>
2025-03-06 20:43:40 -0800

    feat: new window for the simulator, with a back button to go back to main menu

--- Canvas.java
+++ Canvas.java
@@ -1,10 +1,13 @@
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 
 
 
@@ -20,22 +23,62 @@ class Canvas {
     }
 
     public void menu() {
+        Canvas canvas = new Canvas();
 
         JPanel panel = new JPanel();
-        frame.getContentPane();
+        canvas.frame.getContentPane();
         
         JLabel label = new JLabel("Gravity Simulation");
         Dimension size = label.getPreferredSize();
-        label.setBounds(150,100, size.width, size.height);
-        frame.add(label);
+        label.setBounds(100, 100, size.width, size.height);
+        panel.add(label);
 
         JButton startButton = new JButton("Start");
-        frame.add(startButton);
-        frame.add(panel);
-        frame.setVisible(true); // set the frame to be true
+        startButton.setBounds(100, 100, size.width, size.height);
+        panel.add(startButton);
+        canvas.frame.add(panel);
+        canvas.frame.setVisible(true); // set the frame to be true
+
+        startButton.addActionListener(new ActionListener() {
+            // Code inside actionPerformed will run whenever startButton is clicked
+            // This method can only be called when actionPerformed
+            public void actionPerformed(ActionEvent e) {
+                // open simulator canvas
+                canvas.simulator();
+            }
+        }
+        ); 
         
 
     }
-    
+
+    public void simulator() {
+        Canvas canvas = new Canvas();
+
+        JPanel panel = new JPanel();
+        canvas.frame.getContentPane();
+        
+        JButton backButton = new JButton("Back");
+        backButton.setBounds(100, 100, 100, 100);
+        panel.add(backButton);
+        backButton.addActionListener(new ActionListener() {
+            // Code inside actionPerformed will run whenever startButton is clicked
+            // This method can only be called when actionPerformed
+            public void actionPerformed(ActionEvent e) {
+                // open main menu canvas
+                canvas.menu();
+            }
+        }
+        );
+
+        JLabel label2 = new JLabel("velocity = ");
+        Dimension size2 = label2.getPreferredSize();
+        label2.setBounds(100, 100, size2.width, size2.height);
+        panel.add(label2);
+
+        canvas.frame.add(panel);
+        canvas.frame.setVisible(true);
+
+    }
 
 }
